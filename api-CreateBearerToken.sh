9628e4209b1b87b2b3839e5bd90dbd798f6c6caa
Robert Schroeder <robjschroeder@gmail.com>
2022-02-22 21:38:51 -0700

    Update api-CreateBearerToken.sh

--- api-CreateBearerToken.sh
+++ api-CreateBearerToken.sh
@@ -1,21 +1,45 @@
-#!/bin/bash
+#!/bin/zsh
+#
+# Script will generate a bearer token for use
+# with JPAPI
+#
+# Updated: 2.22.2022 @ Robjschroeder                 
+#
+# Variables
+#
+# API credentials
+apiUsername=""
+apiPassword=""
+jssURL=""
+#
+# Check for input on variables, prompt if empty
+#
+# Empty Username
+if [ -z ${apiUsername} ]; then
+	echo "Please enter your Jamf Pro username: "
+	read $apiUsername
+fi
+# Empty Password
+if [ -z ${apiPassword} ]; then
+	echo "Please enter your Jamf Pro password: "
+	read $apiPassword
+fi
+# Empty jssURL
+if [ -z ${jssURL} ]; then
+	echo "Please enter your Jamf Pro URL: "
+	echo "(ex. https://server.jamfcloud.com)"
+	read $jssURL
+fi
 
-#Add API credentials
-username="apiUser"
-password="apiPassword"
-URL="https://server.jamfcloud.com"
+# Create base64-encoded credentials
+encodedCredentials=$( printf "${apiUsername}:${apiPassword}" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
 
-# created base64-encoded credentials
-encodedCredentials=$( printf "$username:$password" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
-
-# generate an auth token
-authToken=$( /usr/bin/curl "$URL/uapi/auth/tokens" \
+# Generate an auth token
+authToken=$( /usr/bin/curl "${jssURL}/uapi/auth/tokens" \
 --silent \
 --request POST \
---header "Authorization: Basic $encodedCredentials" )
+--header "Authorization: Basic ${encodedCredentials}" )
 
-echo $authToken
+token=$( /usr/bin/awk -F \" '{ print $4 }' <<< "${authToken}" | /usr/bin/xargs )
 
-token=$( /usr/bin/awk -F \" '{ print $4 }' <<< "$authToken" | /usr/bin/xargs )
-
-echo $token
+echo ${token}
