8eb6cd7a4c26b461050fc9ba4ccfb40323dfaea5
Robert Schroeder <robjschroeder@gmail.com>
2022-02-22 21:47:07 -0700

    Update api-DeleteJamfProUser.sh

--- api-DeleteJamfProUser.sh
+++ api-DeleteJamfProUser.sh
@@ -1,12 +1,43 @@
-#!/bin/bash
-
-#Add API credentials
-username="apiUser"
-password="apiPassword"
-URL="https://server.jamfcloud.com"
-
-# Find the id of the account you would like to delete
+#!/bin/zsh
+#
+# Script will generate a bearer token for use
+# with JPAPI
+#
+# Updated: 2.22.2022 @ Robjschroeder                 
+#
+# Variables
+#
+# API credentials
+apiUsername=""
+apiPassword=""
+jssURL=""
+#
+# ID of the account you would like to delete
 id=""
+#
+# Check for input on variables, prompt if empty
+#
+# Empty Username
+if [ -z ${apiUsername} ]; then
+	echo "Please enter your Jamf Pro username: "
+	read $apiUsername
+fi
+# Empty Password
+if [ -z ${apiPassword} ]; then
+	echo "Please enter your Jamf Pro password: "
+	read $apiPassword
+fi
+# Empty jssURL
+if [ -z ${jssURL} ]; then
+	echo "Please enter your Jamf Pro URL: "
+	echo "(ex. https://server.jamfcloud.com)"
+	read $jssURL
+fi
+# Empty id
+if [ -z ${id} ]; then
+  echo "Please enter the id number of the account you want to delete: "
+  read $id
+fi
 
 # Command to delete the account
 curl -su $username:$password $URL/JSSResource/accounts/userid/$id -X DELETE
