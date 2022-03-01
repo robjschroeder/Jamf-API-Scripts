ec14be35d3c2b8a4f9b73fb685171f06c6668b3c
Robert Schroeder <robjschroeder@gmail.com>
2022-03-01 08:36:57 -0700

    Update api-PopulateEaWithUsername.sh

--- api-PopulateEaWithUsername.sh
+++ api-PopulateEaWithUsername.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
+
 # This script will pass the username from the mobile devices mobile record into a separate EA field.
 
 #Add API credentials
-username="user"
-password="pass"
-URL="https://server.jamfcloud.com"
+username="apiUsername"
+password="apiPassword"
+URL="https://jamf.pro.com"
 
+encodedCredentials=$( printf "${username}:${password}" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
+
+# generate an auth token
+authToken=$( /usr/bin/curl "${URL}/uapi/auth/tokens" \
+--silent \
+--request POST \
+--header "Authorization: Basic ${encodedCredentials}" )
+
+# parse authToken for token, omit expiration
+token=$( /usr/bin/awk -F \" '{ print $4 }' <<< "$authToken" | /usr/bin/xargs )
+
+#Get
 # Enter the EA ID number that you would like to populate
-EA=""
+EA="5"
 
-#Get Token from Jamf Pro
-token=$(printf "${username}:${password}" | iconv -t ISO-8859-1 | base64 -i -)
-
-
-
-ids+=($(curl -s -H "accept: text/xml" -H "authorization: Basic $token" -S $URL/JSSResource/mobiledevices -X GET | xmllint --format - | awk -F'>|<' '/<id>/{print $3}' | sort -n))
+ids+=($(curl -s -H "accept: text/xml" -H "authorization: Bearer ${token}" -S ${URL}/JSSResource/mobiledevices -X GET | xmllint --format - | awk -F'>|<' '/<id>/{print $3}' | sort -n))
 
 for id in "${ids[@]}"; do
-	deviceUser=$(curl -s -H "accept: text/xml" -H "authorization: Basic $token" -S $URL/JSSResource/mobiledevices/id/$id -X GET | xmllint --xpath "/mobile_device/location/username/text()" -)
-	if [[ ! -z $deviceUser ]];then
-		curl -s -H "content-type: text/xml" -H "authorization: Basic $token" -S $URL/JSSResource/mobiledevices/id/$id -X PUT -d "<mobile_device><extension_attributes><extension_attribute><id>$EA</id><value>$deviceUser</value></extension_attribute></extension_attributes></mobile_device>"
+	deviceUser=$(curl -s -H "accept: text/xml" -H "authorization: Bearer ${token}" -S ${URL}/JSSResource/mobiledevices/id/${id} -X GET | xmllint --xpath "/mobile_device/location/username/text()" -)
+	if [[ ! -z ${deviceUser} ]];then
+		curl -s -H "content-type: text/xml" -H "authorization: Bearer ${token}" -S ${URL}/JSSResource/mobiledevices/id/${id} -X PUT -d "<mobile_device><extension_attributes><extension_attribute><id>${EA}</id><value>${deviceUser}</value></extension_attribute></extension_attributes></mobile_device>"
 	fi
 done
+
+exit 0
