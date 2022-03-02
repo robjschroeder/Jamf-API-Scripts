fa9a57d6489d903ef3cd28199023a4968f509a6d
Robert Schroeder <robjschroeder@gmail.com>
2022-03-02 12:53:00 -0700

    Create api-redeployJamfFramework.sh

---
+++ api-redeployJamfFramework.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Redeploys the Jamf Management Framework 
+# for enrolled device
+#
+# Updated: 3.02.2022 @ Robjschroeder  
+
+# Variables
+
+# Add API credentials
+username="apiUsername"
+password="apiPassword"
+URL="https://jamf.pro.com"
+
+# Instance id of computer
+id=""
+
+
+encodedCredentials=$( printf "${username}:${password}" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
+
+# Generate an auth token
+authToken=$( /usr/bin/curl "${URL}/uapi/auth/tokens" \
+--silent \
+--request POST \
+--header "Authorization: Basic ${encodedCredentials}" )
+
+# Parse authToken for token, omit expiration
+token=$( /usr/bin/awk -F \" '{ print $4 }' <<< "$authToken" | /usr/bin/xargs )
+
+curl --request POST \
+     --url https://${URL}.jamfcloud.com/api/v1/jamf-management-framework/redeploy/${id} \
+     --header 'Accept: application/json'
+     --heater 'Authorization: Bearer ${token}'
+
+exit 0
