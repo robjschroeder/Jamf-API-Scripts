53d66048b8a40f79efd1150b3b5d4610f9fd9eed
Robert Schroeder <robjschroeder@gmail.com>
2022-05-23 10:06:55 -0700

    Update api-InitializeCSAToken.sh

--- api-InitializeCSAToken.sh
+++ api-InitializeCSAToken.sh
@@ -6,47 +6,74 @@
 #
 # Created 4.26.2022 @robjschroeder
 #
+# Updated 5.23.2022 @robjschroeder
+## Added functions to the script
 
-# Variables
+##################################################
+# Variables -- edit as needed
+
+# Jamf Pro API Credentials
+jamfProAPIUsername="api_Username"
+jamfProAPIPassword="api_Password"
+jamfProURL="https://server.jamfcloud.com"
+
+# Jamf Nation Credentials
+JNEmail="jamfNationUser@email.com"
+JNPassword="jn_Password1234"
+
 #
-# Add API credentials
-username="api_Username"
-password="api_Password"
-URL="https://server.jamfcloud.com/"
+##################################################
+# Functions -- do not edit below here
 
-# CSA Token Credentials
-csa_username="csa_Username"
-csa_password="csa_Password"
-
-encodedCredentials=$( printf "${username}:${password}" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
-
-# Generate an auth token
-authToken=$( /usr/bin/curl "${URL}/uapi/auth/tokens" \
---silent \
---request POST \
---header "Authorization: Basic ${encodedCredentials}" 
+getBearerToken(){
+	# Encode credentials
+	encodedCredentials=$( printf "${jamfProAPIUsername}:${jamfProAPIPassword}" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
+	
+	# Generate an auth token
+	authToken=$( /usr/bin/curl "${jamfProURL}/uapi/auth/tokens" \
+		--silent \
+		--request POST \
+		--header "Authorization: Basic ${encodedCredentials}" 
 )
+	
+	# Parse authToken for token, omit expiration
+	token=$( /usr/bin/awk -F \" '{ print $4 }' <<< "${authToken}" | /usr/bin/xargs )
+}
 
-# Parse authToken for token, omit expiration
-token=$( /usr/bin/awk -F \" '{ print $4 }' <<< "${authToken}" | /usr/bin/xargs )
+invalidateToken(){
+	curl --request POST \
+	--url ${jamfProURL}/api/v1/auth/invalidate-token \
+	--header 'Accept: application/json' \
+	--header "Authorization: Bearer ${token}"
+}
 
-
-curl --request POST \
---url ${URL}/api/v1/csa/token \
---header 'Accept: application/json' \
---header 'Content-Type: application/json' \
---header "Authorization: Bearer ${token}" \
---data '
+# Initialize the CSA token exchange
+createCSA(){
+	curl --request POST \
+	--url ${jamfProURL}/api/v1/csa/token \
+	--header 'Accept: application/json' \
+	--header 'Content-Type: application/json' \
+	--header "Authorization: Bearer ${token}" \
+	--data '
 {
-	"emailAddress": "${csa_username}",
-	"password": "${csa_password}"
+	"emailAddress": "'"${JNEmail}"'",
+	"password": "'"${JNPassword}"'"
 }
 '
+}
 
-# Invalidate the token
-curl --request POST \
---url ${URL}/api/v1/auth/invalidate-token \
---header 'Accept: application/json' \
---header "Authorization: Bearer ${token}"
+#
+##################################################
+# Script Work
+#
+#
+# Initialize Cloud Services Connection
+if [ -z ${JNEmail} || -z ${JNPassword} ]; then
+	echo "Jamf Nation credentials not defined, please verify"
+else
+	getBearerToken
+	createCSA
+	invalidateToken
+fi
 
 exit 0
