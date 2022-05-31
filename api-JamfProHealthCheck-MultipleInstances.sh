a26e519bc66c8ed029fcbae31ea593433ff680d1
Robert Schroeder <robjschroeder@gmail.com>
2022-05-31 10:17:57 -0700

    Create api-JamfProHealthCheck-MultipleInstances.sh

---
+++ api-JamfProHealthCheck-MultipleInstances.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Get the Jamf Pro Startup Status for
+# Jamf Pro Server
+
+# Variables -- edit these based on needs
+#
+# Add API credentials
+username="apiUsername"
+password="apiPassword"
+
+
+# Jamf Pro Instances Array
+instances=(
+	https://server1.jamfcloud.com
+	https://server2.jamfcloud.com
+	https://server3.jamfcloud.com
+	https://server4.jamfcloud.com
+	https://server5.jamfcloud.com
+)
+
+#
+##################################################
+# Script Work -- do not edit below here
+while :
+do
+# Loop through each instance in the 
+# instance array
+for inst in ${instances[@]}; do
+	URL="${inst}"
+	JPInstance=$( echo ${URL} | sed 's|^http[s]://||g' | sed 's/\..*//' | tr '[a-z]' '[A-Z]' )
+	
+	# Returns curl health check error from Jamf Pro if found
+	echo -n "Status ${JPInstance} Server Health Check ===>"
+	curl -k -m 5 -s ${inst}/healthCheck.html
+	serverstat01=$(echo $?)
+	# do not put anything between the curl command above and the variable that collects the output
+	if [ ! $serverstat01 = '0' ]; then
+		echo "Checking again: ${JPInstance}"
+		echo -n "Status ${JPInstance} Server check 2 ===>"
+		curl -k -m 5 -s ${inst}/healthCheck.html
+		serverstat01=$(echo $?)
+		if [ ! $serverstat01 = '0' ]; then
+			echo -n "${JPInstance}: ERROR from curl is: $serverstat01"
+		fi
+	fi
+done
+done
+exit 0
