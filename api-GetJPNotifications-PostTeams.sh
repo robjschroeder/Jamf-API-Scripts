4053c143ed8a11ea104f9518e116f6bc0af97187
robjschroeder <robjschroeder@gmail.com>
2022-07-08 10:37:27 -0700

    Updating script

--- api-GetJPNotifications-PostTeams.sh
+++ api-GetJPNotifications-PostTeams.sh
@@ -5,30 +5,25 @@
 # the results to a Microsoft Teams channel
 # via a webhook
 #
-# Updated: 3.28.2022 @ Robjschroeder  
+# Updated: 3.28.2022 @ Robjschroeder
 #
-# Variables
-#
-# Add API credentials
-username="apiUsername"
-password="apiPassword"
-URL="https://server.jamfcloud.com"
-JPInstance=$( echo ${URL} | sed 's|^http[s]://||g' | sed 's/\..*//' | tr '[a-z]' '[A-Z]' )
+# Updated 07.08.2022 @robjschroeder
+
+
+##################################################
+# Variables -- edit as needed
+
+# Jamf Pro API Credentials
+jamfProAPIUsername=""
+jamfProAPIPassword=""
+jamfProURL=""
+
+# Token declarations
+token=""
+tokenExpirationEpoch="0"
 
 # Webhook for Teams created with https://docs.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook
 TeamsWebhookURL=""
-
-encodedCredentials=$( printf "${username}:${password}" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
-
-# Generate an auth token
-authToken=$( /usr/bin/curl "${URL}/uapi/auth/tokens" \
---silent \
---request POST \
---header "Authorization: Basic ${encodedCredentials}" 
-)
-
-# Parse authToken for token, omit expiration
-token=$( /usr/bin/awk -F \" '{ print $4 }' <<< "${authToken}" | /usr/bin/xargs )
 
 # Build an array of possible important notifications from Jamf Pro
 noticationsArr=(
@@ -89,52 +84,134 @@ noticationsArr=(
 	AZURE_AD_MIGRATION_REPORT_GENERATED
 )
 
-# Returns important notifications from Jamf Pro in JSON
-data=$( curl --request GET \
---url "${URL}/api/v1/notifications" \
+#
+##################################################
+# Functions -- do not edit below here
+
+# Get a bearer token for Jamf Pro API Authentication
+getBearerToken(){
+	# Encode credentials
+	encodedCredentials=$( printf "${jamfProAPIUsername}:${jamfProAPIPassword}" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
+	authToken=$(/usr/bin/curl -s -H "Authorization: Basic ${encodedCredentials}" "${jamfProURL}"/api/v1/auth/token -X POST)
+	token=$(/bin/echo "${authToken}" | /usr/bin/plutil -extract token raw -)
+	tokenExpiration=$(/bin/echo "${authToken}" | /usr/bin/plutil -extract expires raw - | /usr/bin/awk -F . '{print $1}')
+	tokenExpirationEpoch=$(/bin/date -j -f "%Y-%m-%dT%T" "${tokenExpiration}" +"%s")
+}
+
+checkVariables(){
+	# Checking for Jamf Pro API variables
+	if [ -z $jamfProAPIUsername ]; then
+		echo "Please enter your Jamf Pro Username: "
+		read -r jamfProAPIUsername
+	fi
+	
+	if [  -z $jamfProAPIPassword ]; then
+		echo "Please enter your Jamf Pro password for $jamfProAPIUsername: "
+		read -r -s jamfProAPIPassword
+	fi
+	
+	if [ -z $jamfProURL ]; then
+		echo "Please enter your Jamf Pro URL (with no slash at the end): "
+		read -r jamfProURL
+	fi
+	
+	# Checking for additional variables
+	# Teams Webhook URL
+	if [ $TeamsWebhookURL = "" ]; then
+		read -p "Please enter your Teams Webhook URL: " TeamsWebhookURL
+	fi
+}
+
+checkTokenExpiration() {
+	nowEpochUTC=$(/bin/date -j -f "%Y-%m-%dT%T" "$(/bin/date -u +"%Y-%m-%dT%T")" +"%s")
+	if [[ tokenExpirationEpoch -gt nowEpochUTC ]]
+	then
+		/bin/echo "Token valid until the following epoch time: " "${tokenExpirationEpoch}"
+	else
+		/bin/echo "No valid token available, getting new token"
+		getBearerToken
+	fi
+}
+
+# Invalidate the token when done
+invalidateToken(){
+	responseCode=$(/usr/bin/curl -w "%{http_code}" -H "Authorization: Bearer ${token}" ${jamfProURL}/api/v1/auth/invalidate-token -X POST -s -o /dev/null)
+	if [[ ${responseCode} == 204 ]]
+	then
+		/bin/echo "Token successfully invalidated"
+		token=""
+		tokenExpirationEpoch="0"
+	elif [[ ${responseCode} == 401 ]]
+	then
+		/bin/echo "Token already invalid"
+	else
+		/bin/echo "An unknown error occurred invalidating the token"
+	fi
+}
+
+getNotifications(){
+	JPInstance=$( echo ${jamfProURL} | sed 's|^http[s]://||g' | sed 's/\..*//' | tr '[a-z]' '[A-Z]' )
+	
+	# Returns important notifications from Jamf Pro in JSON
+	data=$( curl --request GET \
+--url "${jamfProURL}/api/v1/notifications" \
 --header "Accept: application/json" \
 --header "Authorization: Bearer ${token}" \
 )
-
-# Loop each notification possibility and if it
-# exists in the results, grab information on the
-# notification using jq and notify
-for str in ${noticationsArr[@]}; do
-	if [[ " ${data} " =~ ${str} ]]; then
-		# Create a clean string because the webhook doesn't like "_"
-		cleanString=$( echo ${str} | sed 's/_/ /g' )
-		name=$( echo ${data} | jq --arg v "${str}" '.[]|select(.type==$v).params.name')
-		# Create a clean name because the webhook doesn't like """
-		cleanname=$( echo ${name} | sed 's/"//g')
-		days=$( echo ${data} | jq --arg v "${str}" '.[]|select(.type==$v).params.days')
-		id=$( echo ${data} | jq --arg v "${str}" '.[]|select(.type==$v).params.id')
-		if [[ ${name} != "null" ]]; then
-			if [[ ${days} != "null" ]]; then
-				if [[ ${id} != "null" ]]; then
-					message=$( echo "${JPInstance} Notification: ${cleanString} for ${cleanname} in ${days} days" )
-				else
-					message=$( echo "${JPInstance} Notification: ${cleanString} for ${cleanname} in ${days} days" )
-				fi
-			else
-				message=$( echo "${JPInstance} Notification: ${cleanString} for ${name}" )
-			fi
-		else
-			message=$( echo "${JPInstance} Notification: ${cleanString}" )
-		fi
-		# Generate data to post to Teams chat
-		generate_post_data()
-		{
-			cat <<EOF
+	
+	# Loop each notification possibility and if it
+	# exists in the results, grab information on the
+	# notification using jq and notify
+	for str in ${noticationsArr[@]}; do
+		if [[ " ${data} " =~ ${str} ]]; then
+			# Create a clean string because the webhook doesn't like "_"
+			cleanString=$( echo ${str} | sed 's/_/ /g' )
+			name=$( echo ${data} | jq --arg v "${str}" '.[]|select(.type==$v).params.name')
+			# Create a clean name because the webhook doesn't like """
+			cleanname=$( echo ${name} | sed 's/"//g')
+			days=$( echo ${data} | jq --arg v "${str}" '.[]|select(.type==$v).params.days')
+			id=$( echo ${data} | jq --arg v "${str}" '.[]|select(.type==$v).params.id')
+			if [[ ${name} != "null" ]]; then
+				if [[ ${days} != "null" ]]; then
+					if [[ ${id} != "null" ]]; then
+						message=$( echo "${JPInstance} Notification: ${cleanString} for ${cleanname} in ${days} days" )
+							else
+							message=$( echo "${JPInstance} Notification: ${cleanString} for ${cleanname} in ${days} days" )
+								fi
+								else
+								message=$( echo "${JPInstance} Notification: ${cleanString} for ${name}" )
+									fi
+									else
+									message=$( echo "${JPInstance} Notification: ${cleanString}" )
+									fi
+									# Generate data to post to Teams chat
+									generate_post_data()
+									{
+										cat <<EOF
 {
 	"text":"$message"
 }
 EOF
-		}
-		
-		# Send message to Teams Webhook
-		curl -H "Content-Type: application/json" -d "$(generate_post_data)" $TeamsWebhookURL
-		
-	fi
-done
+									}
+									
+									# Send message to Teams Webhook
+									curl -H "Content-Type: application/json" -d "$(generate_post_data)" $TeamsWebhookURL
+									
+									fi
+								done
 	
+}
+
+#
+##################################################
+# Script Work
+#
+#
+# Calling all functions
+
+checkVariables 
+checkTokenExpiration
+getNotifications 
+invalidateToken
+
 exit 0
--- api-GetManagedUnManagedJPStats-MultipleTenants.sh
+++ api-GetManagedUnManagedJPStats-MultipleTenants.sh
@@ -5,52 +5,82 @@
 #
 # Created: 4.29.2022 @ Robjschroeder
 #
+# Updated 07.08.2022 @robjschroeder
+
 ##################################################
-# Variables -- edit these based on needs
-#
-# Add API credentials
-username="api_Username"
-password="api_Password"
-#
+# Variables -- edit as needed
+
+# Jamf Pro API Credentials
+jamfProAPIUsername=""
+jamfProAPIPassword=""
+
+# Token declarations
+token=""
+tokenExpirationEpoch="0"
+
 # Jamf Pro Instances Array
 instances=(
 	https://server1.jamfcloud.com
 	https://server2.jamfcloud.com
 	https://server3.jamfcloud.com
-	https://server6.jamfcloud.com
-	https://server9.onlinepcm.com
-	https://server5.jamfcloud.com
 	https://server4.jamfcloud.com
-	https://servera.jamfcloud.com
-	https://server1b.jamfcloud.com
-	https://server22.jamfcloud.com
-	https://server7.jamfcloud.com
-	https://server12.jamfcloud.com
+	https://server5.jamfcloud.com
 )
+
 #
 ##################################################
 # Functions -- do not edit below here
 
+# Get a bearer token for Jamf Pro API Authentication
 getBearerToken(){
 	# Encode credentials
-	encodedCredentials=$( printf "${username}:${password}" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
-	
-	# Generate an auth token
-	authToken=$( /usr/bin/curl "${URL}/uapi/auth/tokens" \
-		--silent \
-		--request POST \
-		--header "Authorization: Basic ${encodedCredentials}" 
-)
-	
-	# Parse authToken for token, omit expiration
-	token=$( /usr/bin/awk -F \" '{ print $4 }' <<< "${authToken}" | /usr/bin/xargs )
+	encodedCredentials=$( printf "${jamfProAPIUsername}:${jamfProAPIPassword}" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
+	authToken=$(/usr/bin/curl -s -H "Authorization: Basic ${encodedCredentials}" "${jamfProURL}"/api/v1/auth/token -X POST)
+	token=$(/bin/echo "${authToken}" | /usr/bin/plutil -extract token raw -)
+	tokenExpiration=$(/bin/echo "${authToken}" | /usr/bin/plutil -extract expires raw - | /usr/bin/awk -F . '{print $1}')
+	tokenExpirationEpoch=$(/bin/date -j -f "%Y-%m-%dT%T" "${tokenExpiration}" +"%s")
 }
 
+checkVariables(){
+	# Checking for Jamf Pro API variables
+	if [ -z $jamfProAPIUsername ]; then
+		echo "Please enter your Jamf Pro Username: "
+		read -r jamfProAPIUsername
+	fi
+	
+	if [  -z $jamfProAPIPassword ]; then
+		echo "Please enter your Jamf Pro password for $jamfProAPIUsername: "
+		read -r -s jamfProAPIPassword
+	fi
+	
+	# Checking for additional variables
+}
+
+checkTokenExpiration() {
+	nowEpochUTC=$(/bin/date -j -f "%Y-%m-%dT%T" "$(/bin/date -u +"%Y-%m-%dT%T")" +"%s")
+	if [[ tokenExpirationEpoch -gt nowEpochUTC ]]
+	then
+		/bin/echo "Token valid until the following epoch time: " "${tokenExpirationEpoch}"
+	else
+		/bin/echo "No valid token available, getting new token"
+		getBearerToken
+	fi
+}
+
+# Invalidate the token when done
 invalidateToken(){
-	curl --request POST \
-	--url ${URL}/api/v1/auth/invalidate-token \
-	--header 'Accept: application/json' \
-	--header "Authorization: Bearer ${token}"
+	responseCode=$(/usr/bin/curl -w "%{http_code}" -H "Authorization: Bearer ${token}" ${jamfProURL}/api/v1/auth/invalidate-token -X POST -s -o /dev/null)
+	if [[ ${responseCode} == 204 ]]
+	then
+		/bin/echo "Token successfully invalidated"
+		token=""
+		tokenExpirationEpoch="0"
+	elif [[ ${responseCode} == 401 ]]
+	then
+		/bin/echo "Token already invalid"
+	else
+		/bin/echo "An unknown error occurred invalidating the token"
+	fi
 }
 
 sortInstances(){
@@ -58,45 +88,53 @@ sortInstances(){
 	instancesArr=$(echo ${instances[@]} | tr ' ' '\n' | sort)
 }
 
-##################################################
-# Script work
-#
-# Sort the instances
-sortInstances 
-
-# Loop through each instance in the 
-# instance array
-for inst in ${instancesArr[@]}; do
-	URL="${inst}"
-	JPInstance=$( echo ${URL} | sed 's|^http[s]://||g' | sed 's/\..*//' | tr '[a-z]' '[A-Z]' )
-	
-	getBearerToken
-
-	# Returns inventory information from Jamf Pro in JSON
-	data=$( curl --request GET \
+getInventory(){
+	# Loop through each instance in the 
+	# instance array
+	for inst in ${instancesArr[@]}; do
+		jamfProURL="${inst}"
+		JPInstance=$( echo ${jamfProURL} | sed 's|^http[s]://||g' | sed 's/\..*//' | tr '[a-z]' '[A-Z]' )
+		
+		checkTokenExpiration 
+		
+		# Returns inventory information from Jamf Pro in JSON
+		data=$( curl --request GET \
 				--silent \
-				--url ${URL}/api/v1/inventory-information \
+				--url ${jamfProURL}/api/v1/inventory-information \
 				--header "Accept: application/json" \
 				--header "Authorization: Bearer ${token}" \
 				--output -
 )
-	# Store specific counts into variables
-	managedComputers=$( echo ${data} | jq '.managedComputers')
-	unmanagedComputers=$( echo ${data} | jq '.unmanagedComputers')
-	managedDevices=$( echo ${data} | jq '.managedDevices')
-	unmanagedDevices=$( echo ${data} | jq '.unmanagedDevices')
-	totalCount=$((${unmanagedDevices} + ${managedDevices} + ${unmanagedComputers} + ${managedComputers}))
-	
-	echo ${JPInstance}
-	echo "Managed Computers: ${managedComputers}"
-	echo "Unmanaged Computers: ${unmanagedComputers}"
-	echo "Managed Devices: ${managedDevices}"
-	echo "Unmanaged Devices: ${unmanagedDevices}"
-	echo "Total count: $totalCount"
-	echo "--------------------------"
-	
-	invalidateToken 
+		# Store specific counts into variables
+		managedComputers=$( echo ${data} | jq '.managedComputers')
+		unmanagedComputers=$( echo ${data} | jq '.unmanagedComputers')
+		managedDevices=$( echo ${data} | jq '.managedDevices')
+		unmanagedDevices=$( echo ${data} | jq '.unmanagedDevices')
+		totalCount=$((${unmanagedDevices} + ${managedDevices} + ${unmanagedComputers} + ${managedComputers}))
+		
+		echo ${JPInstance}
+		echo "Managed Computers: ${managedComputers}"
+		echo "Unmanaged Computers: ${unmanagedComputers}"
+		echo "Managed Devices: ${managedDevices}"
+		echo "Unmanaged Devices: ${unmanagedDevices}"
+		echo "Total count: $totalCount"
+		echo "--------------------------"
+		
+		invalidateToken 
+		
+	done
+}
 
-done
+#
+##################################################
+# Script Work
+#
+#
+# Calling all functions
+
+checkVariables 
+sortInstances 
+getInventory 
+invalidateToken
 
 exit 0
