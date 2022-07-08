16da68487d61018452b5561de639b0fe6a67445c
robjschroeder <robjschroeder@gmail.com>
2022-07-08 13:55:29 -0700

    Create api-UpdateReEnrollmentSettigns.sh

---
+++ api-UpdateReEnrollmentSettigns.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+# Update the Re-enrollment object
+#
+# Created 07.08.2022 @robjschroeder
+
+##################################################
+# Variables -- edit as needed
+
+# Jamf Pro API Credentials
+jamfProAPIUsername=""
+jamfProAPIPassword=""
+jamfProURL=""
+
+# Token declarations
+token=""
+tokenExpirationEpoch="0"
+
+#
+##################################################
+# Functions -- do not edit below here
+
+# Get a bearer token for Jamf Pro API Authentication
+getBearerToken(){
+	# Encode credentials
+	encodedCredentials=$( printf "${jamfProAPIUsername}:${jamfProAPIPassword}" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
+	authToken=$(/usr/bin/curl -s -H "Authorization: Basic ${encodedCredentials}" "${jamfProURL}"/api/v1/auth/token -X POST)
+	token=$(/bin/echo "${authToken}" | /usr/bin/plutil -extract token raw -)
+	tokenExpiration=$(/bin/echo "${authToken}" | /usr/bin/plutil -extract expires raw - | /usr/bin/awk -F . '{print $1}')
+	tokenExpirationEpoch=$(/bin/date -j -f "%Y-%m-%dT%T" "${tokenExpiration}" +"%s")
+}
+
+checkVariables(){
+	# Checking for Jamf Pro API variables
+	if [ -z $jamfProAPIUsername ]; then
+		echo "Please enter your Jamf Pro Username: "
+		read -r jamfProAPIUsername
+	fi
+	
+	if [  -z $jamfProAPIPassword ]; then
+		echo "Please enter your Jamf Pro password for $jamfProAPIUsername: "
+		read -r -s jamfProAPIPassword
+	fi
+	
+	if [ -z $jamfProURL ]; then
+		echo "Please enter your Jamf Pro URL (with no slash at the end): "
+		read -r jamfProURL
+	fi
+	
+	# Checking for additional variables
+}
+
+checkTokenExpiration() {
+	nowEpochUTC=$(/bin/date -j -f "%Y-%m-%dT%T" "$(/bin/date -u +"%Y-%m-%dT%T")" +"%s")
+	if [[ tokenExpirationEpoch -gt nowEpochUTC ]]
+	then
+		/bin/echo "Token valid until the following epoch time: " "${tokenExpirationEpoch}"
+	else
+		/bin/echo "No valid token available, getting new token"
+		getBearerToken
+	fi
+}
+
+# Invalidate the token when done
+invalidateToken(){
+	responseCode=$(/usr/bin/curl -w "%{http_code}" -H "Authorization: Bearer ${token}" ${jamfProURL}/api/v1/auth/invalidate-token -X POST -s -o /dev/null)
+	if [[ ${responseCode} == 204 ]]
+	then
+		/bin/echo "Token successfully invalidated"
+		token=""
+		tokenExpirationEpoch="0"
+	elif [[ ${responseCode} == 401 ]]
+	then
+		/bin/echo "Token already invalid"
+	else
+		/bin/echo "An unknown error occurred invalidating the token"
+	fi
+}
+
+# Update ReEnrollment Settings
+updateReEnrollment(){
+	echo "$(date '+%A %W %Y %X'): Updating ReEnrollment settings..."
+	curl --request PUT \
+	--url ${jamfProURL}/api/v1/reenrollment \
+	--header 'Accept: application/json' \
+	--header 'Content-Type: application/json' \
+	--header "Authorization: Bearer ${token}" \
+	--data '
+{
+	"isFlushPolicyHistoryEnabled": true,
+	"isFlushLocationInformationEnabled": true,
+	"isFlushLocationInformationHistoryEnabled": true,
+	"isFlushExtensionAttributesEnabled": true,
+	"flushMDMQueue": "DELETE_EVERYTHING"
+}
+'
+}
+
+#
+##################################################
+# Script Work
+#
+#
+# Calling all functions
+
+checkVariables 
+checkTokenExpiration
+updateReEnrollment 
+invalidateToken
+
+exit 0
